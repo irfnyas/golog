fe3edd30a038ab735e2d5597a6c350e4f6b416c3
Irfan Yas <irfanyasiras@gmail.com>
2023-07-15 09:29:55 +0700

    update test

--- test/golog_test.dart
+++ test/golog_test.dart
@@ -1,1 +1,15 @@
-void main() {}
+import 'package:flutter_test/flutter_test.dart';
+import 'package:golog/golog.dart';
+
+void main() {
+  test('add new log', () {
+    const title = 'Log Test';
+    const body = '{"hello": "world"}';
+
+    Golog.add(title, body: body);
+
+    expect(Golog.logLength.value, 1);
+    expect(Golog.logList.first.title, title);
+    expect(Golog.logList.first.body, body);
+  });
+}
