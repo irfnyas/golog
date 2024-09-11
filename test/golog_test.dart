734eccedf0d7c91607292939363eab1f8fcf4ae9
Irfan Yas <irfanyasiras@gmail.com>
2024-09-11 10:11:52 +0700

    v-0-1-3-2

--- test/golog_test.dart
+++ test/golog_test.dart
@@ -5,9 +5,9 @@ void main() {
   test('add new log', () {
     const title = 'Log Test';
     const body = {'hello': 'world'};
+    final model = GologModel(title: title, body: body);
 
-    Golog.add(title, body: body);
-
+    Golog.list().add(model);
     expect(Golog.list().length, 1);
     expect(Golog.list().first.title, title);
     expect(Golog.list().first.body, body);
